4a3cce572ab28834bd7c0ade6cbd8f249334995c
Andreas Mautsch <amautsch>
2023-06-30 08:42:08 +0200

    refactoring

--- src/main/java/org/goafabric/catalogservice/Application.java
+++ src/main/java/org/goafabric/catalogservice/Application.java
@@ -1,9 +1,6 @@
 package org.goafabric.catalogservice;
 
 import io.micrometer.observation.ObservationPredicate;
-import org.goafabric.catalogservice.service.repository.entity.ChargeItemEo;
-import org.goafabric.catalogservice.service.repository.entity.DiagnosisEo;
-import org.goafabric.catalogservice.service.repository.entity.InsuranceEo;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,11 +11,8 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClas
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportRuntimeHints;
-import org.springframework.data.relational.core.mapping.event.BeforeConvertCallback;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.UUID;
 
 
 /**
@@ -50,46 +44,9 @@ public class Application {
     @Bean
     ObservationPredicate disableHttpServerObservationsFromName() { return (name, context) -> !name.startsWith("spring.security."); }
 
-    @Bean
-    BeforeConvertCallback<ChargeItemEo> beforeConvertCallback1() {
-        return aggregate -> {
-            if (aggregate.id == null) aggregate.id = UUID.randomUUID().toString();
-            return aggregate;
-        };
-    }
-
-    @Bean
-    BeforeConvertCallback<DiagnosisEo> beforeConvertCallback2() {
-        return aggregate -> {
-            if (aggregate.id == null) aggregate.id = UUID.randomUUID().toString();
-            return aggregate;
-        };
-    }
-    @Bean
-    BeforeConvertCallback<InsuranceEo> beforeConvertCallback3() {
-        return aggregate -> {
-            if (aggregate.id == null) aggregate.id = UUID.randomUUID().toString();
-            return aggregate;
-        };
-    }
-
-    /*
-    @Bean
-    NamingStrategy namingStrategy() {
-        return new NamingStrategy() {
-            @Override
-            public String getSchema() {
-                return "catalog";
-            }
-        };
-    }
-
-     */
-
     static class ApplicationRuntimeHints implements RuntimeHintsRegistrar {
         @Override
         public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
-            hints.resources().registerPattern("en/*.yml"); //needed for stupid faker
             hints.resources().registerPattern("catalogs/*.csv");
         }
     }
