7fdb635423b9eb0355d91aea63c95173924e561f
Andreas Mautsch <amautsch>
2023-09-16 08:35:49 +0200

    - java 20 compatibility + jvm baseimage updated to java 20 - upgrade to Spring Boot 3.1.3 - various dependency upgrades

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,19 +5,20 @@ version = "1.0.4-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 val dockerRegistry = "goafabric"
-val nativeBuilder = "dashaun/builder:20230225"
-val baseImage = "ibm-semeru-runtimes:open-17.0.6_10-jre-focal@sha256:739eab970ff538cf22a20b768d7755dad80922a89b73b2fddd80dd79f9b880a1" //"eclipse-temurin:20_36-jdk-ubi9-minimal"
+val graalvmBuilderImage = "ghcr.io/graalvm/native-image-community:20.0.2" //"ghcr.io/graalvm/native-image-community:17.0.8"
+val baseImage = "ibm-semeru-runtimes:open-20.0.1_9-jre-focal@sha256:f1a10da50d02f51e79e3c9604ed078a39c19cd2711789cab7aa5d11071482a7e"
+jacoco.toolVersion = "0.8.10"
 
 plugins {
 	java
 	jacoco
-	id("org.springframework.boot") version "3.1.2"
+	id("org.springframework.boot") version "3.1.3"
 	id("io.spring.dependency-management") version "1.1.0"
-	id("org.graalvm.buildtools.native") version "0.9.23"
-	id("com.google.cloud.tools.jib") version "3.3.1"
+	id("org.graalvm.buildtools.native") version "0.9.24"
+	id("com.google.cloud.tools.jib") version "3.3.2"
 }
 
-jacoco { toolVersion = "0.8.9" }
+jacoco { toolVersion = "0.8.10" }
 
 repositories {
 	mavenCentral()
@@ -54,7 +55,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 
 	//persistence
-	//implementation("org.springframework.boot:spring-boot-starter-data-jpa") {exclude("org.glassfish.jaxb", "jaxb-runtime")}
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 	implementation("com.h2database:h2")
 	implementation("org.postgresql:postgresql")
@@ -88,14 +88,22 @@ jib {
 	from.platforms.set(listOf(amd64, arm64))
 }
 
-tasks.register("dockerImageNative") { group = "build"; dependsOn("bootBuildImage") }
-tasks.named<BootBuildImage>("bootBuildImage") {
-	val nativeImageName = "${dockerRegistry}/${project.name}-native" + (if (System.getProperty("os.arch").equals("aarch64")) "-arm64v8" else "") + ":${project.version}"
-	builder.set(nativeBuilder)
-	imageName.set(nativeImageName)
-	environment.set(mapOf("BP_NATIVE_IMAGE" to "true", "BP_JVM_VERSION" to "17", "BP_NATIVE_IMAGE_BUILD_ARGUMENTS" to "-J-Xmx4000m"))
-	doLast {
-		exec { commandLine("docker", "run", "--rm", nativeImageName, "-check-integrity") }
-		exec { commandLine("docker", "push", nativeImageName) }
+buildscript { dependencies { classpath("com.google.cloud.tools:jib-native-image-extension-gradle:0.1.0") }}
+tasks.register("dockerImageNativeNoTest") {group = "build"; dependsOn("bootJar")
+	jib.to.image = ""
+	doFirst {
+		exec { commandLine(
+			"docker", "run", "--rm", "--mount", "type=bind,source=${projectDir}/build,target=/build", "--entrypoint", "/bin/bash", graalvmBuilderImage, "-c", """ mkdir -p /build/native/nativeCompile && cp /build/libs/*-SNAPSHOT.jar /build/native/nativeCompile && cd /build/native/nativeCompile && jar -xvf *.jar &&
+			native-image -J-Xmx5000m -march=compatibility -H:Name=application $([[ -f META-INF/native-image/argfile ]] && echo @META-INF/native-image/argfile) -cp .:BOOT-INF/classes:$(ls -d -1 "/build/native/nativeCompile/BOOT-INF/lib/"*.* | tr "\n" ":") && /build/native/nativeCompile/application -check-integrity """
+		)}
+		jib.from.image = "ubuntu:22.04"
+		jib.to.image = "${dockerRegistry}/${project.name}-native" + (if (System.getProperty("os.arch").equals("aarch64")) "-arm64v8" else "") + ":${project.version}"
+		jib.pluginExtensions { pluginExtension {properties = mapOf("imageName" to "application"); implementation = "com.google.cloud.tools.jib.gradle.extension.nativeimage.JibNativeImageExtension" }}
 	}
+	finalizedBy("jib")
+}
+tasks.register("dockerImageNative") {group = "build"; dependsOn("clean", "dockerImageNativeNoTest"); doLast { exec { commandLine("docker", "run", "--rm", "--pull", "always", "${dockerRegistry}/${project.name}-native" + (if (System.getProperty("os.arch").equals("aarch64")) "-arm64v8" else "") + ":${project.version}", "-check-integrity") } } }
+
+graalvmNative { //https://graalvm.github.io/native-build-tools/latest/gradle-plugin.html#configuration-options
+	binaries.named("main") { quickBuild.set(true) }
 }
--- doc/release-notes.md
+++ doc/release-notes.md
@@ -1,4 +1,7 @@
 # 1.0.4
+- java 20 compatibility + jvm baseimage updated to java 20
+- upgrade to Spring Boot 3.1.3
+- various dependency upgrades
 
 # 1.0.3
 - renamed Diagnosis to Condition
