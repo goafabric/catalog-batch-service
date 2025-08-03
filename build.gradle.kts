45bdaf91c477795b1efa01f97f3cda738032b8d9
Andreas Mautsch <amautsch>
2025-08-03 07:20:48 +0200

    boot 4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -107,4 +107,4 @@ openApi {
 	tasks.forkedSpringBootRun { dependsOn("compileAotJava", "processAotResources") }
 }
 
-rewrite { activeRecipe("UpgradeSpringBoot_4_0") }
+rewrite { activeRecipe("UpgradeSpringBoot_4_0", "UpgradeSpringBatch_6_0") }
