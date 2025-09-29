84e5f2e505ec3ef34bb6cdae4929f5ac614e7b01
Andreas Mautsch <amautsch>
2025-09-29 10:35:44 +0200

    chang

--- src/main/java/org/goafabric/catalog/job/chargeitem/ChargeItemRepository.java
+++ src/main/java/org/goafabric/catalog/job/chargeitem/ChargeItemRepository.java
@@ -1,13 +1,14 @@
 package org.goafabric.catalog.job.chargeitem;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface ChargeItemRepository extends CrudRepository<ChargeItemEo, String> {
-    //@Query("SELECT * FROM catalog.chargeitem WHERE code = :code")
+    @Query("SELECT * FROM catalog.chargeitem WHERE code = :code")
     ChargeItemEo findByCode(String code);
 
-    //@Query("SELECT * FROM catalog.chargeitem WHERE UPPER(display) LIKE CONCAT(UPPER(:display), '%')")
+    @Query("SELECT * FROM catalog.chargeitem WHERE UPPER(display) LIKE CONCAT(UPPER(:display), '%')")
     List<ChargeItemEo> findByDisplayStartsWithIgnoreCase(String display);
 }
--- src/main/java/org/goafabric/catalog/job/condition/ConditionRepository.java
+++ src/main/java/org/goafabric/catalog/job/condition/ConditionRepository.java
@@ -1,11 +1,15 @@
 package org.goafabric.catalog.job.condition;
 
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface ConditionRepository extends CrudRepository<ConditionEo, String> {
+    @Query("SELECT * FROM catalog.condition WHERE code = :code")
     ConditionEo findByCode(String code);
+
+    @Query("SELECT * FROM catalog.condition WHERE UPPER(display) LIKE CONCAT(UPPER(:display), '%')")
     List<ConditionEo> findByDisplayStartsWithIgnoreCase(String display);
 }
