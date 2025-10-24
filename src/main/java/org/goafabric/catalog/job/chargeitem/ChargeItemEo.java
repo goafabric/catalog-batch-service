98365478d9e5affd2eaf7f7dc2012afa0a3360d7
Andreas Mautsch <amautsch>
2025-10-24 09:54:52 +0200

    boot 4.0

--- src/main/java/org/goafabric/catalog/job/chargeitem/ChargeItemEo.java
+++ src/main/java/org/goafabric/catalog/job/chargeitem/ChargeItemEo.java
@@ -2,13 +2,12 @@ package org.goafabric.catalog.job.chargeitem;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Version;
-import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.util.UUID;
 
 @Table(name = "chargeitem", schema = "catalog")
-@Document("chargeitem")
+//@Document("chargeitem")
 public record ChargeItemEo(
     @Id String id,
     @Version Long version,
--- src/main/java/org/goafabric/catalog/job/chargeitem/ChargeItemImportConfiguration.java
+++ src/main/java/org/goafabric/catalog/job/chargeitem/ChargeItemImportConfiguration.java
@@ -4,13 +4,13 @@ import org.goafabric.catalog.job.JobCompletionListener;
 import org.goafabric.catalog.job.StepCompletionListener;
 import org.springframework.batch.core.job.Job;
 import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.job.parameters.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.Step;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.infrastructure.item.ItemReader;
+import org.springframework.batch.infrastructure.item.ItemWriter;
+import org.springframework.batch.infrastructure.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -29,9 +29,9 @@ public class ChargeItemImportConfiguration {
 
     @Bean
     public Step chargeItemStep(ItemReader<ChargeItemEo> diagnosisItemReader,
-                           ItemWriter<ChargeItemEo> chargeItemEoItemWriter,
-                           JobRepository jobRepository,
-                           PlatformTransactionManager ptm) {
+                               ItemWriter<ChargeItemEo> chargeItemEoItemWriter,
+                               JobRepository jobRepository,
+                               PlatformTransactionManager ptm) {
         return new StepBuilder("chargeItemStep", jobRepository)
                 .<ChargeItemEo, ChargeItemEo>chunk(2, ptm)
                 .reader(diagnosisItemReader)
