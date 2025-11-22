2b1664b2d64cee9600de85596fb58205a4fcfd07
Andreas Mautsch <amautsch>
2025-11-22 10:33:36 +0100

    boot 4.0

--- src/main/java/org/goafabric/catalog/Application.java
+++ src/main/java/org/goafabric/catalog/Application.java
@@ -2,11 +2,8 @@ package org.goafabric.catalog;
 
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
@@ -15,11 +12,6 @@ public class Application {
 
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner init(ApplicationContext context) {
-        return args -> {if ((args.length > 0) && ("-check-integrity".equals(args[0]))) {SpringApplication.exit(context, () -> 0);}};
     }
 
     static class ApplicationRuntimeHints implements RuntimeHintsRegistrar {
