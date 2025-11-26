eab88fe87a3cd1b7dfb41dc77c65e02554542bc7
Andreas Mautsch <amautsch>
2025-11-26 09:01:57 +0100

    boot 4.0

--- src/main/java/org/goafabric/catalog/job/JobRepositoryConfiguration.java
+++ src/main/java/org/goafabric/catalog/job/JobRepositoryConfiguration.java
@@ -2,7 +2,7 @@ package org.goafabric.catalog.job;
 
 import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
 import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
+import org.springframework.batch.core.repository.support.JdbcJobRepositoryFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -16,7 +16,7 @@ public class JobRepositoryConfiguration extends DefaultBatchConfiguration {
 
     @Bean
     public JobRepository jobRepository(DataSource dataSource, PlatformTransactionManager transactionManager) throws Exception {
-        JobRepositoryFactoryBean factory = new JobRepositoryFactoryBean();
+        JdbcJobRepositoryFactoryBean factory = new JdbcJobRepositoryFactoryBean();
         factory.setDataSource(dataSource);
         factory.setTransactionManager(transactionManager);
         factory.setIsolationLevelForCreate("ISOLATION_READ_COMMITTED");
