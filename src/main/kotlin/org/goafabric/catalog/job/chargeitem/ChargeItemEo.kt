367078ce58df402dbd93a7f8ad71cb40a92cc130
Andreas Mautsch <amautsch>
2025-12-20 11:05:59 +0100

    kotlin

--- src/main/kotlin/org/goafabric/catalog/job/chargeitem/ChargeItemEo.kt
+++ src/main/kotlin/org/goafabric/catalog/job/chargeitem/ChargeItemEo.kt
@@ -1,14 +1,14 @@
 package org.goafabric.catalog.job.chargeitem
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.Version
 import org.springframework.data.relational.core.mapping.Table
 import java.util.*
 
 @Table(name = "chargeitem", schema = "catalog") //@Document("chargeitem")
 data class ChargeItemEo(
-    @Id
-    val id: String = UUID.randomUUID().toString(),
-    val version: Long? = null,
+    @Id val id: String = UUID.randomUUID().toString(),
+    @Version val version: Long? = null,
     val code: String?,
     val display: String?,
     val price: Double?
--- src/main/kotlin/org/goafabric/catalog/job/condition/ConditionEo.kt
+++ src/main/kotlin/org/goafabric/catalog/job/condition/ConditionEo.kt
@@ -1,14 +1,14 @@
 package org.goafabric.catalog.job.condition
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.Version
 import org.springframework.data.relational.core.mapping.Table
 import java.util.*
 
 @Table(name = "condition", schema = "catalog") //@Document("condition")
 data class ConditionEo(
-    @Id
-    val id: String = UUID.randomUUID().toString(),
-    val version: Long? = null,
+    @Id val id: String = UUID.randomUUID().toString(),
+    @Version val version: Long? = null,
     val code: String?,
     val display: String?,
     val shortname: String?
